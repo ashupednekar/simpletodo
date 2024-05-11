0bdde6293006718c453417b98c07b04c0c3f4ef4
ashupednekar <ashutoshpednekar15@gmail.com>
2024-05-11 12:28:55 +0530

    feat: added lipgloss join horizontal, multiple lists

--- kanban/model.go
+++ kanban/model.go
@@ -5,21 +5,37 @@ import (
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type Model struct {
-	list list.Model
-	err  error
+	focused status
+	loaded  bool
+	lists   []list.Model
+	err     error
 }
 
 func (m *Model) InitList(height int, width int) {
-	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
-	m.list.Title = "TODO"
-	m.list.SetItems([]list.Item{
+	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
+	defaultList.SetShowHelp(false)
+	m.lists = []list.Model{defaultList, defaultList, defaultList}
+	m.lists[todo].Title = "To Do"
+	m.lists[todo].SetItems([]list.Item{
 		Task{status: todo, title: "get up", description: "get up now"},
 		Task{status: todo, title: "freshen up", description: "daily routine"},
 		Task{status: todo, title: "get dressed", description: "wear clothes"},
 		Task{status: todo, title: "go to work", description: "get in the car and drive"},
+	})
+
+	m.lists[inProgress].Title = "In Progress"
+	m.lists[inProgress].SetItems([]list.Item{
+		Task{status: inProgress, title: "cook", description: "cook food"},
+		Task{status: inProgress, title: "have lunch", description: "have food"},
+	})
+
+	m.lists[complete].Title = "Complete"
+	m.lists[complete].SetItems([]list.Item{
+		Task{status: complete, title: "attend dsm", description: "attend standup meeting"},
 	})
 }
 
@@ -30,16 +46,27 @@ func (m Model) Init() tea.Cmd {
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.InitList(msg.Height, msg.Width)
+		if !m.loaded {
+			m.InitList(msg.Height, msg.Width)
+			m.loaded = true
+		}
 	}
 
 	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
+	m.lists[m.focused], cmd = m.lists[m.focused].Update(msg)
 	return m, cmd
 }
 
 func (m Model) View() string {
-	return m.list.View()
+	if m.loaded {
+		return lipgloss.JoinHorizontal(lipgloss.Left,
+			m.lists[todo].View(),
+			m.lists[inProgress].View(),
+			m.lists[complete].View(),
+		)
+	} else {
+		return "Loading, pls wait..."
+	}
 }
 
 func StartKanban() {
